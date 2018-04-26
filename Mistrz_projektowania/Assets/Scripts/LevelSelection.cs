f0f45dfd9d4f3725583cbfefc83c2114fe8745e5
martynaa0123 <martynaa0123@wp.pl>
2018-04-26 02:56:36 +0200

    First commit
    Added Unity project to GitHub

---
+++ Mistrz_projektowania/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour {
+
+    public void selectLevel() {
+        switch (this.gameObject.name) {
+            case "Level1Button":
+                SceneManager.LoadScene("Level1");
+                break;
+            case "Level2Button":
+                SceneManager.LoadScene("Level2");
+                break;
+
+        }
+
+
+      
+
+
+
+    }
+}
