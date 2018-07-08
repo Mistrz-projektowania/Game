0a8f62360435c81e2fe06aac9bf6493ffcf0b669
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-07-08 20:30:43 +0200

    Tworzenie systemu ładowania danych z plików XML

---
+++ Mistrz_projektowania/Assets/Scripts/XMLDataLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+public class XMLDataLoader : MonoBehaviour {
+	public TextAsset Database;
+
+	List<Dictionary<string,string>> Trips = new List<Dictionary<string,string>>();
+	Dictionary<string,string> obj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GetData(){
+		XmlDocument xmlDoc = new XmlDocument(); // xmlDoc is the new xml document.
+		xmlDoc.LoadXml(Database.text); // load the file.
+		XmlNodeList levelsList = xmlDoc.GetElementsByTagName("Zleceniodawca"); 
+	}
+}
