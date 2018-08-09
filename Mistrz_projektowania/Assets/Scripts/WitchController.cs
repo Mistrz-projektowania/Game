fb2cea6b0c4f18d4369c912514514c4cc87c1096
infernalnephilim <xmacabrax@gmail.com>
2018-08-09 14:59:39 +0200

    Dodanie miotły (wstępny model) i ruchu czarownicy (idle)

---
+++ Mistrz_projektowania/Assets/Scripts/WitchController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchController : MonoBehaviour {
+
+	private Vector3 startPos;
+	float speed = 2;
+	float rotateSpeed = 100;
+	float xScale = 0.1f;
+	float yScale = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fly ();
+	}
+
+	void fly(){
+		transform.Rotate (Vector3.right * Mathf.Sin(Time.timeSinceLevelLoad/2*speed)*xScale - Vector3.up * Mathf.Sin(Time.timeSinceLevelLoad * speed)*yScale);
+		transform.position = startPos + (Vector3.right * Mathf.Sin(Time.timeSinceLevelLoad/2*speed)*xScale - Vector3.up * Mathf.Sin(Time.timeSinceLevelLoad * speed)*yScale);
+	}
+}
