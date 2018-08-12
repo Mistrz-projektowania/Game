1ecb9746676de90a36d40c65a84113f9393e3e81
martynaa0123 <martynaa0123@wp.pl>
2018-08-12 21:19:05 +0200

    wybór postaci ukończenie

---
+++ Mistrz_projektowania/Assets/Scripts/ChooseCharacter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseCharacter : MonoBehaviour
+{
+
+    private int person;
+    private GameObject[] characters;
+
+    private void Start()
+    {
+
+        characters = new GameObject[transform.childCount];
+        person = PlayerPrefs.GetInt("Selected");
+
+        for (int i = 0; i < transform.childCount; i++)
+
+            characters[i] = transform.GetChild(i).gameObject;
+
+        foreach (GameObject all in characters)
+            all.SetActive(false);
+
+        if (characters[person])
+            characters[person].SetActive(true);
+    }
+
+    public void ChooseNatalia()
+    {
+        characters[person].SetActive(false);
+        person++;
+        if (person > characters.Length)
+            person = 1;
+        characters[person].SetActive(true);
+    }
+
+    public void ChooseŁukasz()
+    {
+
+        characters[person].SetActive(false);
+        person--;
+        if (person < 0)
+            person = 0;
+        characters[person].SetActive(true);
+    }
+
+
+    public void SelectButton()
+    {
+        PlayerPrefs.SetInt("Selected", person);
+        SceneManager.LoadScene("Level1");
+    }
+}
