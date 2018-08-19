6cf6a45a25e3a4d62f8d75567c71df69fd26b9ae
martynaa0123 <martynaa0123@wp.pl>
2018-08-19 17:05:57 +0200

    puzzle c.d.

--- Mistrz_projektowania/Assets/Scripts/GameManager.cs
+++ Mistrz_projektowania/Assets/Scripts/GameManager.cs
@@ -2,34 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public Puzzle puzzlePrefab;
     private List<Puzzle> puzzleParts = new List<Puzzle>();
     public Transform PuzzlePanel;
-   
-    private Vector2 position = new Vector2(100f,290f);
-    private Vector2 space = new Vector2(2f, 1.42f);
 
-    
+    private Vector2 startPos = new Vector2(-6f, 2.77f);
+    private Vector2 space = new Vector2(2.76f, 2f);
+
+
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
         PuzzleGame(8);
         StartPosition();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void PuzzleGame(int parts)
     {
         for (int i = 0; i < parts; i++)
         {
-            puzzleParts.Add(Instantiate(puzzlePrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, PuzzlePanel) );
-            
+            puzzleParts.Add(Instantiate(puzzlePrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, PuzzlePanel));
+
 
         }
     }
@@ -37,16 +40,16 @@ public class GameManager : MonoBehaviour {
     void StartPosition()
     {
 
-        puzzleParts[0].transform.position = new Vector3(position.x, position.y, 0.0f);
-        puzzleParts[1].transform.position = new Vector3(202 + space.x, position.y, 0.0f);
-      //  puzzleParts[2].transform.position = new Vector3(position.x + (2* offset.x), position.y, 0.0f);
+        puzzleParts[0].transform.position = new Vector3(startPos.x, startPos.y, 0.0f);
+        puzzleParts[1].transform.position = new Vector3(startPos.x + space.x, startPos.y, 0.0f);
+        //  puzzleParts[2].transform.position = new Vector3(position.x + (2* offset.x), position.y, 0.0f);
 
-        puzzleParts[2].transform.position = new Vector3(position.x, 219 - space.y, 0.0f);
-        puzzleParts[3].transform.position = new Vector3(202 + space.x, 219 - space.y, 0.0f);
-        puzzleParts[4].transform.position = new Vector3(304 + (2*space.x), 219 - space.y, 0.0f);
+        puzzleParts[2].transform.position = new Vector3(startPos.x, startPos.y - space.y, 0.0f);
+        puzzleParts[3].transform.position = new Vector3(startPos.x + space.x, startPos.y - space.y, 0.0f);
+        puzzleParts[4].transform.position = new Vector3(startPos.x + (2 * space.x), startPos.y - space.y, 0.0f);
 
-        puzzleParts[5].transform.position = new Vector3(position.x, 148 - (2 * space.y), 0.0f);
-       puzzleParts[6].transform.position = new Vector3(202 + space.x, 148 - (2 * space.y), 0.0f);
-        puzzleParts[7].transform.position = new Vector3(304 + (2 * space.x), 148 - (2 * space.y), 0.0f);
+        puzzleParts[5].transform.position = new Vector3(startPos.x, startPos.y - (2 * space.y), 0.0f);
+        puzzleParts[6].transform.position = new Vector3(startPos.x + space.x, startPos.y - (2 * space.y), 0.0f);
+        puzzleParts[7].transform.position = new Vector3(startPos.x + (2 * space.x), startPos.y - (2 * space.y), 0.0f);
     }
 }
