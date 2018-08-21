e52e1af2d51d16b24c099fbf7f3ee043f04ebebe
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-08-21 20:53:17 +0200

    Zmiana wielkości pól z danymi

--- Mistrz_projektowania/Assets/Scripts/setSlotPosition.cs
+++ Mistrz_projektowania/Assets/Scripts/setSlotPosition.cs
@@ -16,7 +16,7 @@ public class setSlotPosition : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		pos = Camera.main.WorldToScreenPoint(parentObject.transform.position);
+		pos = Camera.main.WorldToScreenPoint(parentObject.transform.position - new Vector3(0,0.5f,0));
 		myObject.transform.position = new Vector3(pos.x, pos.y, 0);
 	}
 }
