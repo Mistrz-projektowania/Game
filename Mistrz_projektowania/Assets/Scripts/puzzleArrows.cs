ecd8f673db71aa1095fa2f555f6c0034e526e176
martynaa0123 <martynaa0123@wp.pl>
2019-07-04 02:05:13 +0200

    puzzle menu
    puzzle menu

---
+++ Mistrz_projektowania/Assets/Scripts/puzzleArrows.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class puzzleArrows : MonoBehaviour {
+
+	private void OnMouseDown () {
+		if (gameObject.name == "LeftArrow") {
+			SceneManager.LoadScene ("PuzzleMenu");
+		}
+
+		if (gameObject.name == "RightArrow") {
+			SceneManager.LoadScene ("PuzzleMenuSecond");
+		}
+
+	}
+}
---
+++ Mistrz_projektowania/Assets/Scripts/puzzleMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class puzzleMenu : MonoBehaviour {
+
+	private GameObject leftArrow;
+	private GameObject rightArrow;
+
+	void Start() {
+		leftArrow = GameObject.Find("LeftArrow");
+		rightArrow = GameObject.Find("RightArrow");
+
+		if (SceneManager.GetActiveScene ().name == "PuzzleMenu") {
+			leftArrow.SetActive (false);
+		}
+
+		if (SceneManager.GetActiveScene ().name == "PuzzleMenuSecond") {
+			rightArrow.SetActive (false);
+		}
+	}
+
+	void Update() {
+	}
+}
