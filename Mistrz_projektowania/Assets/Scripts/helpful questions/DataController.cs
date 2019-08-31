6e3a767e971924d25fde6f441929d9c99b2780b4
martynaa0123 <martynaa0123@wp.pl>
2019-08-31 16:25:54 +0200

    quiz

--- Mistrz_projektowania/Assets/Scripts/helpful questions/DataController.cs
+++ Mistrz_projektowania/Assets/Scripts/helpful questions/DataController.cs
@@ -10,42 +10,35 @@ public class DataController : MonoBehaviour
     public Quiz[] allLevels;
     public GameObject QuestionPanel;
     public GameObject questions;
-    private string gameDataFileName = "data.json";
+	private string fileWithData = "quiz_questions_and_answers.json";
 
     public void Start()
     {
       //  DontDestroyOnLoad(gameObject);
-		SceneManager.LoadScene("Helpful_questions");
-	//	LoadFromFile();
+		//SceneManager.LoadScene("Helpful_questions");
+		LoadFromFile();
        //questions.SetActive(false);
        // QuestionPanel.SetActive(true);
 
-          
     }
+
+	private void LoadFromFile() {
+		string filePath = Path.Combine(Application.streamingAssetsPath, fileWithData);
+		if (File.Exists(filePath))
+		{
+			string dataAsJson = File.ReadAllText(filePath);
+			QuizData loadedData = JsonUtility.FromJson<QuizData>(dataAsJson);
+
+			allLevels = loadedData.allLevels;
+		}
+		else
+		{
+			Debug.LogError("nie można załadować danych z pliku");
+		}
+	}
 
     public Quiz GetCurrentLevel()
     {
 		return allLevels[0];
-    }
-
-
-    void Update()
-    {
-
-    }
-
-    private void LoadFromFile() {
-        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
-        if (File.Exists(filePath))
-        {
-            string dataAsJson = File.ReadAllText(filePath);
-            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
-
-			allLevels = loadedData.allLevels;
-        }
-        else
-        {
-            Debug.LogError("nie można załadować danych z pliku");
-        }
     }
 }
